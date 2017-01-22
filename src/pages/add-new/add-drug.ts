bfeac71950fcf8821f341bfa9b45f6e2aadff4d5
Sayem Hossain <sayemkcn@gmail.com>
2017-01-23 00:33:39 +0600

    added new drugs, showed drug list on front page

--- src/pages/add-new/add-drug.ts
+++ src/pages/add-new/add-drug.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { ViewController } from 'ionic-angular';
+import {Storage} from '@ionic/storage';
 
 // import {Drug} from '../../model/drug';
 import {IDrug} from '../../model/idrug';
@@ -8,17 +9,19 @@ import {IDrug} from '../../model/idrug';
 @Component({
   selector: 'page-add-drug',
   templateUrl: 'add-drug.html',
-  providers : [IDrug]
+  providers : [IDrug,Storage]
 })
 export class AddDrug {
   numbers : number[];
+  druglist : IDrug[];
   
-  constructor(public viewCtrl: ViewController, private drug : IDrug) {
-  
-  }
+  constructor(private viewCtrl: ViewController,
+              private drug : IDrug, 
+              private storage: Storage) {}
 
   addDrugButtonClick(){
     console.log(this.drug);
+    this.saveDrug(this.drug);
   }
 
   dismiss(): void{
@@ -28,7 +31,20 @@ export class AddDrug {
   medicationCountClick(){
     this.numbers = Array(this.drug.medicationCounter).fill(this.drug.medicationCounter).map((x,i)=>i+1)
     this.drug.times = new Array(this.drug.medicationCounter); // intialise array for binding
-    console.log(this.drug);
+  }
+
+  saveDrug(drug : IDrug){
+    this.storage.get("druglist").then(val => {
+      if(val===null){
+        this.druglist = [];
+      }else{
+        this.druglist = val;
+      }
+      this.druglist.push(drug);
+      // val.push(drug);
+      this.storage.set("druglist",this.druglist);
+    });
+    this.viewCtrl.dismiss();
   }
 
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,15 +3,26 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { ModalController } from 'ionic-angular';
 import {AddDrug} from '../add-new/add-drug';
+import {IDrug} from '../../model/idrug';
+import {Storage} from '@ionic/storage';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers : [IDrug, Storage]
 })
 export class HomePage {
+  druglist : IDrug[];
 
-  constructor(public navCtrl: NavController,private modalCtrl: ModalController) {
+  constructor(public navCtrl: NavController,private modalCtrl: ModalController,private storage: Storage) {
 
+  }
+
+  ionViewWillEnter(){
+    this.storage.get("druglist").then(val=>{
+      this.druglist = val;
+      console.log(this.druglist);
+    })
   }
 
   addNewButtonClick(){
