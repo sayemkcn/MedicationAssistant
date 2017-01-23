8563145819ad435d017cffad8020c77d37fe08b1
Sayem Hossain <sayemkcn@gmail.com>
2017-01-23 14:36:50 +0600

    delete item complete

--- src/pages/add-new/add-drug.ts
+++ src/pages/add-new/add-drug.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 import { ViewController } from 'ionic-angular';
-import { NavParams } from 'ionic-angular';
+import { NavParams, LoadingController } from 'ionic-angular';
 import { Storage } from '@ionic/storage';
 
 // import {Drug} from '../../model/drug';
@@ -18,6 +18,7 @@ export class AddDrug {
   paramDrugName : string;
 
   constructor(private viewCtrl: ViewController,
+    private loadingCtrl: LoadingController,
     private navParams: NavParams,
     private drug: IDrug,
     private storage: Storage) {
@@ -27,7 +28,7 @@ export class AddDrug {
 
 
   addDrugButtonClick() {
-    console.log(this.drug);
+    // console.log(this.drug);
     this.saveDrug(this.drug);
   }
 
@@ -41,6 +42,7 @@ export class AddDrug {
   }
 
   saveDrug(drug: IDrug) {
+    this.showLoading();
     this.storage.get("druglist").then(val => {
       if (val === null) {
         this.druglist = [];
@@ -49,13 +51,13 @@ export class AddDrug {
       }
       if (!this.paramDrugName) { // if nevparams is not empty then save new object
         this.druglist.push(drug);
-        console.log(this.druglist);
+        // console.log(this.druglist);
       } else { // edit object
         for (let i = 0; i < this.druglist.length; i++) {
-          if (this.druglist[i].name == this.paramDrugName)
+          if (this.druglist[i] != null && this.druglist[i].name == this.paramDrugName)
             this.druglist[i] = this.drug;
         }
-        console.log(this.druglist);
+        // console.log(this.druglist);
       }
 
       // val.push(drug);
@@ -64,4 +66,15 @@ export class AddDrug {
     this.viewCtrl.dismiss();
   }
 
+  showLoading() {
+    let loading = this.loadingCtrl.create({
+      content: 'Please wait...'
+    });
+
+    loading.present();
+
+    setTimeout(() => {
+      loading.dismiss();
+    }, 2000);
+  }
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,42 +1,91 @@
 import { Component } from '@angular/core';
 
-import { NavController } from 'ionic-angular';
-import { ModalController } from 'ionic-angular';
-import {AddDrug} from '../add-new/add-drug';
-import {IDrug} from '../../model/idrug';
-import {Storage} from '@ionic/storage';
+import { NavController, ModalController, LoadingController, AlertController } from 'ionic-angular';
+import { AddDrug } from '../add-new/add-drug';
+import { IDrug } from '../../model/idrug';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html',
-  providers : [IDrug, Storage]
+  providers: [IDrug, Storage]
 })
 export class HomePage {
-  druglist : IDrug[];
+  druglist: IDrug[];
 
-  constructor(public navCtrl: NavController,private modalCtrl: ModalController,private storage: Storage) {
+  constructor(public navCtrl: NavController,
+    private loadingCtrl: LoadingController,
+    private alertCtrl: AlertController,
+    private modalCtrl: ModalController,
+    private storage: Storage) {
 
   }
 
-  ionViewWillEnter(){
-    this.storage.get("druglist").then(val=>{
+  ionViewWillEnter() {
+    this.storage.get("druglist").then(val => {
       this.druglist = val;
       // console.log(this.druglist);
     })
   }
 
-  addNewButtonClick(){
+  doRefresh(event){
+    setTimeout(()=>{
+      this.ionViewWillEnter();
+      event.complete();
+    },1000);
+  }
+
+  addNewButtonClick() {
     let modal = this.modalCtrl.create(AddDrug);
     modal.present();
   }
 
-  editItem(drug : IDrug){
-    this.navCtrl.push(AddDrug,drug);
+  editItem(drug: IDrug) {
+    this.navCtrl.push(AddDrug, drug);
     // console.log(drug);
   }
 
-  deleteItem(drug : IDrug){
-    console.log(drug);
+  deleteItem(drug: IDrug) {
+    this.showConfirmDeleteDialog(drug);
+  }
+
+  showLoading() {
+    let loading = this.loadingCtrl.create({
+      content: 'Please wait...'
+    });
+
+    loading.present();
+
+    setTimeout(() => {
+      loading.dismiss();
+    }, 2000);
+  }
+
+  showConfirmDeleteDialog(drug: IDrug) {
+    let confirm = this.alertCtrl.create({
+      title: 'Delete?',
+      message: 'Do you really want to delete?',
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: () => {
+            console.log('Disagree clicked');
+          }
+        },
+        {
+          text: 'Delete',
+          handler: () => {
+            for (let i = 0; i < this.druglist.length; i++) {
+              if (this.druglist[i] != null && this.druglist[i].name == drug.name) {
+                this.druglist[i] = null;
+              }
+            }
+            this.storage.set("druglist", this.druglist);
+          }
+        }
+      ]
+    });
+    confirm.present();
   }
 
 }
